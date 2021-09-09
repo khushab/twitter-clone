8503f0f7c32ac6f000b841d2acc8a8ecd1884979
khushab <khushabalam7@gmail.com>
2021-09-09 12:05:07 +0530

    Login page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <template>
-  <div id="app" class="flex container h-screen w-full">
-    <div class="w-1/5 border-r border-lighter px-8 py-2 ">
-      <SidebarLeft />
+  <div>
+    <div id="app" class="flex container h-screen w-full">
+      <div class="w-1/5 border-r border-lighter px-8 py-2">
+        <SidebarLeft />
+      </div>
+      <div class="w-1/2 h-full">
+        <MainSection />
+      </div>
+      <div class="w-1/3 h-full border-l border-lighter px-8 py-2 relative">
+        <SidebarRight />
+      </div>
     </div>
-    
+    <Login />
+    <Signup />
   </div>
 </template>
 
 <script>
-import SidebarLeft from './components/SidebarLeft.vue'
+import SidebarLeft from "./components/SidebarLeft.vue";
+import SidebarRight from "./components/SidebarRight.vue";
+import MainSection from "./components/MainSection.vue";
+import Login from "./components/Login.vue";
+import Signup from "./components/Signup.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    SidebarLeft
-  }
-}
+    SidebarLeft,
+    MainSection,
+    SidebarRight,
+    Login,
+    Signup,
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
