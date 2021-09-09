4ed9fdfa6e0f9adbf1058c835478f36dccd0527a
khushab <khushabalam7@gmail.com>
2021-09-09 12:05:20 +0530

    Signup page

---
+++ src/components/Signup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="h-screen w-full flex flex-col items-center">
+    <form class="w-1/3 px-4">
+      <button
+        class="h-16 w-16 hover:bg-lightblue text-3xl rounded-full text-blue"
+      >
+        <i class="fab fa-twitter"></i>
+      </button>
+      <h1 class="text-3xl font-bold my-4">Create your account</h1>
+      <div class="flex flex-col">
+        <input
+          class="border border-gray-400 my-4 p-3 text-grey-darkest rounded"
+          type="text"
+          required
+          placeholder="Name"
+        />
+        <input
+          class="border border-gray-400 my-4 p-3 text-grey-darkest rounded"
+          type="password"
+          required
+          placeholder="Password"
+        />
+      </div>
+      <button class="bg-blue w-full h-14 rounded-2xl text-xl text-white">
+        Sign up
+      </button>
+      <button class="bg-gray-300 w-full h-14 my-2 rounded-2xl text-xl">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style>
+</style>
