d1bbab90294bd7b7ca78431516b5be4ff0bf54a4
khushab <khushabalam7@gmail.com>
2021-09-09 15:33:44 +0530

    main section and tweet completed

---
+++ src/components/MainSection.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="px-5 py-3 border-b border-lighter flex items-center">
+      <h1 class="text-lg font-bold">Home</h1>
+    </div>
+    <div class="px-5 py-3 border-b border-lighter flex">
+      <div>
+        <img
+          src="https://pbs.twimg.com/profile_images/1283187184028151809/L3SiFEL3_normal.jpg"
+          alt="profile"
+          class="w-12 h-12 rounded-full border border-lighter"
+        />
+      </div>
+      <form class="w-full px-4 relative">
+        <textarea
+          placeholder="What's happening?"
+          class="w-full focus:outline-none mt-3"
+        ></textarea>
+        <div class="flex items-center">
+          <i class="text-lg text-blue mr-4 fas fa-image"></i>
+          <i class="text-lg text-blue mr-4 fas fa-chart-line"></i>
+          <i class="text-lg text-blue mr-4 fas fa-chart-bar"></i>
+          <i class="text-lg text-blue mr-4 fas fa-smile"></i>
+          <i class="text-lg text-blue mr-4 fas fa-tasks"></i>
+        </div>
+        <button
+          class="
+            h-10
+            px-4
+            text-white
+            font-semibold
+            bg-blue
+            hover:bg-darkblue
+            focus:outline-none
+            rounded-full
+            absolute
+            bottom-0
+            right-0
+          "
+        >
+          Tweet
+        </button>
+      </form>
+    </div>
+    <div
+      v-for="tweet in tweets"
+      :key="tweet.id"
+      class="w-full p-4 border-b hover:bg-lighter"
+    >
+      <Tweet :tweet="tweet" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tweet from "./Tweet.vue";
+export default {
+  name: "MainSection",
+  components: {
+    Tweet,
+  },
+  data() {
+    return {
+      tweets: [
+        {
+          src: "https://pbs.twimg.com/profile_images/1423663740344406029/l_-QOIHY_400x400.jpg",
+          name: "Elon Musk",
+          handle: "@elonmusk",
+          tweet: "Should I just quarantine on mars??",
+          id: 1,
+        },
+        {
+          src: "https://pbs.twimg.com/profile_images/1423663740344406029/l_-QOIHY_400x400.jpg",
+          name: "Elon Musk",
+          handle: "@elonmusk",
+          tweet: "Haha just made a flame thrower. Shld I sell them?",
+          id: 2,
+        },
+        {
+          src: "https://pbs.twimg.com/profile_images/1423663740344406029/l_-QOIHY_400x400.jpg",
+          name: "Elon Musk",
+          handle: "@elonmusk",
+          tweet: "Just did something crazyyyyyyy",
+          id: 3,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Tweet.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex">
+    <div class="flex-none mr-4">
+      <img
+        :src="tweet.src"
+        alt="pic"
+        class="h-12 w-12 rounded-full flex-none"
+      />
+    </div>
+    <div class="w-full">
+      <div class="flex items-center w-full">
+        <p class="font-semibold">{{ tweet.name }}</p>
+        <p class="text-sm text-darl ml-2">{{ tweet.handle }}</p>
+      </div>
+      <p>
+        {{ tweet.tweet }}
+      </p>
+      <div
+        class="flex items-center justify-between w-full px-2 my-2 text-black"
+      >
+        <div class="flex items-center cursor-pointer">
+          <i class="mr-2 fas fa-comment"></i>
+        </div>
+        <div class="flex items-center cursor-pointer">
+          <i class="mr-2 fas fa-retweet"></i>
+        </div>
+        <div class="flex items-center cursor-pointer">
+          <i class="mr-2 fas fa-heart"></i>
+        </div>
+        <div class="flex items-center cursor-pointer">
+          <i class="mr-2 fas fa-upload"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tweet",
+  props: ["tweet"],
+  mounted() {
+    console.log(this.tweet);
+  },
+};
+</script>
+
+<style>
+</style>
